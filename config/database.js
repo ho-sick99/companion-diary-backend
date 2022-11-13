cda8921d301efef33b6e3d02a3a56ba192335db7
DESKTOP-EDOFIHE\eun20 <eunkyung20516@gmail.com>
2022-11-14 03:46:41 +0900

    FEAT : Database IP 수정

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '3.38.100.128',
-    port: 13306,
+    host: '3.36.61.143',
+    port: 3306,
     user: 'user',
     password: 'user',
     database: 'COMPAION_DIARY_DB',
