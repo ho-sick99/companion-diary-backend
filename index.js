96e1aa561f4320d617ac767ff24f278b8afb7830
ho-sick99 <jhmoon6807@gmail.com>
2023-03-23 17:00:52 +0900

    https 코드 적용

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ dotenv.config(); // 환경변수 사용
 const http = require('http');
 const https = require('https');
 
-const HTTP_PORT = 3000;
-const HTTPS_PORT = 3333;
+const HTTP_PORT = process.env.SERVER_PORT;
+const HTTPS_PORT = process.env.SECURE_SERVER_PORT;
 
 // // SSL 인증서 파일 불러오기
 // const options = {
@@ -17,22 +17,20 @@ const HTTPS_PORT = 3333;
 //     cert: fs.readFileSync('./rootca.crt')
 //   };
 
-// // HTTP server 가동
-// http.createServer(express).listen(HTTP_PORT, () => {
-//     logger.info(`HTTP Server Start At Port ${HTTP_PORT}`);
-// });
-
-// // HTTPS server 가동
-// https.createServer(options, express).listen(HTTPS_PORT, () => {
-//     logger.info(`HTTPS Server Start At Port ${HTTPS_PORT}`);
-// });
-
-const port = process.env.SERVER_PORT;
-
-express().listen(port, () => {
-    const dir = "./uploads"; // 업로드된 파일을 저장할 경로
-    if (!fs.existsSync(dir)) { // 해당 경로가 존재하지 않는다면
-        fs.mkdirSync(dir); // 폴더 생성
-    }
-    logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
+// HTTP server 가동
+http.createServer(express).listen(HTTP_PORT, () => {
+    logger.info(`HTTP Server Start At Port ${HTTP_PORT}`);
 });
+
+// HTTPS server 가동
+https.createServer(express).listen(HTTPS_PORT, () => {
+    logger.info(`HTTPS Server Start At Port ${HTTPS_PORT}`);
+});
+
+// express().listen(port, () => {
+//     const dir = "./uploads"; // 업로드된 파일을 저장할 경로
+//     if (!fs.existsSync(dir)) { // 해당 경로가 존재하지 않는다면
+//         fs.mkdirSync(dir); // 폴더 생성
+//     }
+//     logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
+// });
