0b60433e2116ff6405265e289ec664398ede628e
ho-sick99 <jhmoon6807@gmail.com>
2023-03-27 17:53:10 +0900

    feat: ssh 터널링을 통한 db 접속 작업중

--- config/database.js
+++ config/database.js
@@ -1,5 +1,16 @@
 const mysql = require('mysql2/promise');    // promise api => ws7 async await와 잘 동작함
 const { logger } = require('./winston');
+const tunnel = require('tunnel-ssh');
+
+// ssh 터널링을 통한 서버 db 접속
+const ssh_config = {
+    username: process.env.SSH_USER,
+    password: process.env.SSH_PASSWORD,
+    host: process.env.SSH_HOST,
+    port: process.env.SSH_PORT,
+    dstHost: process.env.SSH_DATABASE_HOST,
+    dstPort: process.env.SSH_DATABASE_PORT,
+};
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
@@ -12,6 +23,7 @@ const pool = mysql.createPool({
     multipleStatements: true // 다중 쿼리 가능 설정
 });
 
+// 
 // // TODO: 본인의 DB 계정 입력
 // const pool = mysql.createPool({
 //     host: '127.0.0.1',
