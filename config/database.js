ab3c6c851696bc8661df6825ba3472f518378999
ho-sick99 <jhmoon6807@gmail.com>
2023-03-27 15:44:17 +0900

    fix: DB 정보 변경

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2/promise');    // promise api => ws7 async await와 잘 동작함
-const {logger} = require('./winston');
+const { logger } = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '13.125.223.255',
-    port: 3306,
-    user: 'user',
-    password: 'user',
-    database: 'COMPAION_DIARY_DB',
-    charset : 'utf8',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    charset: 'utf8',
     multipleStatements: true // 다중 쿼리 가능 설정
 });
 
